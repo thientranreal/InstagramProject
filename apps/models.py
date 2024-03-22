7fcbfaf073d73fe0b31adfacd429433b7171ad04
Huu Kien <115411396+MakerHowell@users.noreply.github.com>
2024-03-22 10:12:52 +0700

    #sua doi

--- apps/models.py
+++ apps/models.py
@@ -16,7 +16,7 @@ class NguoiDung(models.Model):
     avatar = models.ImageField(blank=True, null=True, upload_to=user_directory_path)
     sobanbe = models.FloatField(blank=True, null=True)
     phone = models.CharField(max_length=15, blank=True, null=True)
-    # is_online = models.BooleanField(default=False)
+    is_online = models.BooleanField(default=False, blank=True, null=True)
 
 class BanBe(models.Model):
     nguoidung1 = models.ForeignKey('NguoiDung', on_delete=models.CASCADE, related_name='ban_be_1')
@@ -53,68 +53,3 @@ class BinhLuan(models.Model):
     noidungbl = models.TextField(blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
-
-
-# Thêm dữ liệu cho lớp NguoiDung
-# for i in range(10):
-#     nguoidung = NguoiDung.objects.create(
-#         ngaysinh=datetime.now(),
-#         sobanbe=10,
-#         phone='0123456789'
-#     )
-#     # Lưu ý: Trường user cần được gán sau khi tạo user
-
-# # Thêm dữ liệu cho lớp BanBe
-# nguoidungs = NguoiDung.objects.all()
-# for i in range(10):
-#     BanBe.objects.create(
-#         nguoidung1=nguoidungs[i],
-#         nguoidung2=nguoidungs[i+1] if i < 9 else nguoidungs[0]
-#     )
-
-# # Thêm dữ liệu cho lớp TinNhan
-# for i in range(10):
-#     TinNhan.objects.create(
-#         senter=nguoidungs[i],
-#         receiver=nguoidungs[i+1] if i < 9 else nguoidungs[0],
-#         noidung=f'Tin nhan thu {i+1}',
-#         thoigian=datetime.now()
-#     )
-
-# # Thêm dữ liệu cho lớp LienLac
-# for i in range(10):
-#     LienLac.objects.create(
-#         goc=nguoidungs[i],
-#         dich=nguoidungs[i+1] if i < 9 else nguoidungs[0],
-#         lastmess=f'Tin nhan cuoi cung giua {nguoidungs[i]} va {nguoidungs[i+1] if i < 9 else nguoidungs[0]}',
-#         thoigiancuoi=datetime.now()
-#     )
-
-# # Thêm dữ liệu cho lớp BaiDang
-# for i in range(10):
-#     BaiDang.objects.create(
-#         nguoidung=nguoidungs[i],
-#         noidung=f'Noi dung bai dang thu {i+1}',
-#         thoigiandang=datetime.now(),
-#         tongluotthich=0,
-#         hinhanh='duongdananh.jpg'
-#     )
-
-# # Thêm dữ liệu cho lớp ThongBao
-# for i in range(10):
-#     ThongBao.objects.create(
-#         nguoidung=nguoidungs[i],
-#         noidung=f'Noi dung thong bao thu {i+1}',
-#         is_read=False,
-#         timestamp=datetime.now()
-#     )
-
-# # Thêm dữ liệu cho lớp BinhLuan
-# baidangs = BaiDang.objects.all()
-# for i in range(10):
-#     BinhLuan.objects.create(
-#         baidang=baidangs[i],
-#         nguoidung=nguoidungs[i],
-#         noidungbl=f'Noi dung binh luan thu {i+1}',
-#         timestamp=datetime.now()
-#     )
